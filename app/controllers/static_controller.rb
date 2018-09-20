fbcd68f7d7c8186c41596d8a1dc563b850c3b449
Scott Weinstein <sweinstein27@gmail.com>
2018-09-20 17:51:01 +0000

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,6 @@ class StaticController < ApplicationController
 
   protect_from_forgery with: :exception
 
-  def new
+  def home
   end
 end
--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,8 +1,15 @@
 class StoreAdminController < ApplicationController
-
-  protect_from_forgery with: :exception
+  layout 'admin'
 
   def home
+  end
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def invoice
+    render :layout => false
   end
 
 end
